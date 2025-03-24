e3106591052ae60d9e2616baa51d51407971b1e8
bobr-ik <aavramenko804@gmail.com>
2025-03-24 17:51:12 +0300

    cors settings

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "http://77.222.54.5"
     ],
     allow_credentials=True,
     allow_methods=["*"],
