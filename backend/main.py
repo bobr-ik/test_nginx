4c78f53fc8fec50c2f49e235733e16b256556b65
bobr-ik <aavramenko804@gmail.com>
2025-03-24 13:50:38 +0300

    fixed back

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from time import time
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ app = FastAPI()
 def ping():
     return {"ans": time()}
 
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
